9d23b3679b3a5306eddbdc703599e8ed19ddeaa6
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2018-06-29 19:28:05 +0300

    Work in progress: support for channels on the DB level

--- postgres/180628-schema.sql
+++ postgres/180628-schema.sql
@@ -19,6 +19,24 @@ CREATE UNIQUE INDEX message_link_idx ON message USING btree (link) ;
 
 -- Drop table
 
+-- DROP TABLE public.notified
+
+CREATE TABLE notified (
+  id serial NOT NULL,
+  subscriber int4 NOT NULL,
+  message int4 NOT NULL,
+  time_notified timestamp NOT NULL,
+  CONSTRAINT notified_pk PRIMARY KEY (id),
+  CONSTRAINT notified_un UNIQUE (subscriber, message),
+  CONSTRAINT notified_message_fk FOREIGN KEY (message) REFERENCES message(id) ON DELETE CASCADE,
+  CONSTRAINT notified_subscriber_fk FOREIGN KEY (subscriber) REFERENCES subscriber(id) ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+) ;
+
+-- Drop table
+
 -- DROP TABLE public.subscriber
 
 CREATE TABLE subscriber (
@@ -40,6 +58,7 @@ CREATE TABLE subscription (
   subscriber int4 NOT NULL,
   protocol varchar NOT NULL,
   target varchar NOT NULL DEFAULT 'default'::character varying,
+  "notify" bool NOT NULL DEFAULT true,
   CONSTRAINT subscription_pk PRIMARY KEY (id),
   CONSTRAINT subscription_un UNIQUE (subscriber, protocol, target),
   CONSTRAINT subscription_subscriber_fk FOREIGN KEY (subscriber) REFERENCES subscriber(id) ON DELETE CASCADE
@@ -50,17 +69,31 @@ WITH (
 
 -- Drop table
 
--- DROP TABLE public.notified
+-- DROP TABLE public.channel
 
-CREATE TABLE notified (
+CREATE TABLE channel (
   id serial NOT NULL,
-  subscriber int4 NOT NULL,
-  message int4 NOT NULL,
-  time_notified timestamp NOT NULL,
-  CONSTRAINT notified_pk PRIMARY KEY (id),
-  CONSTRAINT notified_un UNIQUE (subscriber, message),
-  CONSTRAINT notified_message_fk FOREIGN KEY (message) REFERENCES message(id) ON DELETE CASCADE,
-  CONSTRAINT notified_subscriber_fk FOREIGN KEY (subscriber) REFERENCES subscriber(id) ON DELETE CASCADE
+  "name" varchar NOT NULL,
+  owner int4 NOT NULL,
+  CONSTRAINT channel_pk PRIMARY KEY (id),
+  CONSTRAINT channel_subscriber_fk FOREIGN KEY (owner) REFERENCES subscriber(id) ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+) ;
+
+-- Drop table
+
+-- DROP TABLE public.channel_subscription
+
+CREATE TABLE channel_subscription (
+  id serial NOT NULL,
+  channel int4 NOT NULL,
+  subscription int4 NOT NULL,
+  CONSTRAINT channel_subscription_pk PRIMARY KEY (id),
+  CONSTRAINT channel_subscription_un UNIQUE (channel, subscription),
+  CONSTRAINT channel_subscription_channel_fk FOREIGN KEY (channel) REFERENCES channel(id) ON DELETE CASCADE,
+  CONSTRAINT channel_subscription_subscription_fk FOREIGN KEY (subscription) REFERENCES subscription(id) ON DELETE CASCADE
 )
 WITH (
   OIDS=FALSE

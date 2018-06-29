4c11bfde1075176f1d2c422dda520deb09328d15
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2018-06-29 12:52:55 +0300

    Postgres ports changed to avoid conflict

--- src/main/scala/radar/db/infrastructure.scala
+++ src/main/scala/radar/db/infrastructure.scala
@@ -7,5 +7,5 @@ import doobie._, doobie.implicits._
 object infrastructure {
   implicit lazy val tr: Transactor[IO] =
     Transactor.fromDriverManager[IO](
-      "org.postgresql.Driver", s"jdbc:postgresql://radar_db:5432/postgres", "postgres", "")
+      "org.postgresql.Driver", s"jdbc:postgresql://radar_db:5433/postgres", "postgres", "")
 }
